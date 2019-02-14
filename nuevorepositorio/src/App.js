2132efd31d3ca300ddd4882e5dd49ae071608b5b
unknown <David Muoz>
2019-02-14 11:40:48 -0600

    He termiando la aplicacion local :C

--- nuevorepositorio/src/App.js
+++ nuevorepositorio/src/App.js
@@ -1,19 +1,56 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {listas} from './listas.json';
 import Formulario from './componets/Formulario';
-import Seccion from './componets/Seccion';
 
 
 class App extends Component {
-  render() {
+  constructor(){
+    super();
+    this.state = {listas};
+    this.agregarEnvio = this.agregarEnvio.bind(this);
+  }
 
+  agregarEnvio(lista){
+    this.setState({
+      listas: [...this.state.listas, lista]
+    })
+  }
+
+  render() {
+    const listas = this.state.listas.map((lista, i) => {
+      return(
+        <tr>
+          <td>{lista.codigo}</td>
+          <td>{lista.nombre}</td>
+          <td>{lista.descripcion}</td>
+          <td>{lista.precio}</td>
+          <td>{lista.unidades}</td>
+        </tr>
+      )
+    })
     return (
       <div className="App">
         <h1 className="text-center cafe">Repositorio</h1>
-        <Formulario/><Seccion/>
-
+        <Formulario onAgregar = {this.agregarEnvio} />
+        <section className="col-md-8 inline-block Righ vete-arriba">
+          <h2 className="verde">Lista</h2>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Codigo</th>
+                <th>Nombre</th>
+                <th>Descripcion</th>
+                <th>Precio</th>
+                <th>Unidades</th>
+              </tr>
+            </thead>
+            <tbody>
+              {listas}
+            </tbody>
+          </table>
+        </section>
       </div>
     );
   }
--- nuevorepositorio/src/componets/Formulario.js
+++ nuevorepositorio/src/componets/Formulario.js
@@ -5,45 +5,53 @@ class Formulario extends Component{
 	constructor(){
     super();
     this.state = {
-    	Codigo: '',
-		Nombre: '',
-		Descripcion: '',
-		Precio: 0,
-		Unidades: 0
-    }
+    	codigo: '',
+		nombre: '',
+		descripcion: '',
+		precio: '',
+		unidades: ''
+    };
+    this.Imputchange = this.Imputchange.bind(this);
+    this.Enviar = this.Enviar.bind(this);
   }
 
-	codigo(e){
+	Imputchange(e){
 		const {value, name} = e.target
 		this.setState({
 			[name]:value
 		})
 	}
 
+	Enviar(e){
+		e.preventDefault();
+		console.log(this.state);
+		this.props.onAgregar(this.state);	
+	}
+
 	render(){
 		return(
-			<form className="col-md-4 inline-block para-arriba Righ">
+			<form className="col-md-4 inline-block para-arriba Righ"  onSubmit={this.Enviar}>
 				<h2 className="verde">Formulario</h2>
 				<h3>Ingresa un Producto</h3>
 			 	<div className="form-group">
 					<label>Codigo</label>
-					<input type="text" onChange= {this.codigo.bind(this)} name="codigo" className="form-control" id="control" placeholder="Escribe el Codigo"/>
+					<input type="text" onChange= {this.Imputchange} name="codigo" className="form-control" id="control" placeholder="Escribe el Codigo"/>
 				</div>
 				<div className="form-group">
 					<label>Nombre</label>
-					<input type="text" onChange= {this.codigo.bind(this)} name="nombre" className="form-control" id="nombre" placeholder="Escribe el Nombre"/>
+					<input type="text" onChange= {this.Imputchange} name="nombre" className="form-control" id="nombre" placeholder="Escribe el Nombre"/>
 				</div>
 				<div className="form-group">
 					<label>Descripcion</label>
-					<textarea name="descripcion" onChange= {this.codigo.bind(this)} className="form-control" id="descripcion" placeholder="Escribe una breve descripcion del producto"></textarea>
+					<textarea name="descripcion" onChange= {this.Imputchange} className="form-control" id="descripcion" placeholder="Escribe una breve descripcion del producto"></textarea>
 				</div>
 				<div className="form-group">
 					<label>Precio</label>
-					<input type="Number" onChange= {this.codigo.bind(this)} name="precio" className="form-control" id="precio" placeholder="Escribe el precio"/>
+					<input type="Number" onChange= {this.Imputchange} name="precio" className="form-control" id="precio" placeholder="Escribe el precio"/>
 				</div>
 				<div className="form-group">
 					<label>Unidades</label>
-					<input type="Number" onChange= {this.codigo.bind(this)} name="unidades" className="form-control" id="unidades" placeholder="Escribe la cantida"/>
+					<input type="Number" onChange= {this.Imputchange} name="unidades" className="form-control" id="unidades" placeholder="Escribe la cantida"/>
 				</div>
 				<div className="form-group text-rigth">
 					<input type="submit" className="btn btn-outline-dark" id="boton" value="Guardar"/>
